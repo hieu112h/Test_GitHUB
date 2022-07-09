53e1fac9630218ea74a7bb72c5d08911b534cab3
hieu112h <hieu112h@gmail.com>
2022-07-09 09:17:05 +0700

    test_2

---
+++ Test_2/bai02.java
@@ -0,0 +1,13 @@
+
+public class bai02 {
+
+	public static void main(String[] args) {
+		int number=19;
+		int number2=25;
+		int kq=number2-number+1;
+		int result;
+		result=(int)(Math.random()*kq+number);
+		System.out.printf("%d",result);
+	}
+
+}
